7e7cac255df20b96c4dcdb77cc6e13c40c3c351f
Angel Travieso <angeltraviesoc@gmail.com>
2022-11-29 12:18:20 -0400

    agregando funcionalidades de los botones (play, pausar, retroceder y avanzar musica)

--- app/src/main/java/com/example/musicplayerapp/MainActivity.java
+++ app/src/main/java/com/example/musicplayerapp/MainActivity.java
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     double finalTime = 0;
     int forwardTime = 10000;
     int backwardTime = 10000;
+    static int oneTimeOnly = 0;
 
 
     @Override
@@ -50,7 +55,102 @@ public class MainActivity extends AppCompatActivity {
 
         seekBar.setClickable(false);
 
+        // Adding functionalities for the buttons
 
+
+        // play the music
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playMusic();
+            }
+        });
+
+
+        // pause the music
+        pauseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mediaPlayer.pause();
+            }
+        });
+
+
+        // advance the music 10seconds
+        forwardBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int temp = (int) startTime;
+                if((temp + forwardTime) <= finalTime) {
+                    /*startTime = startTime + forwardTime;*/
+                    startTime += forwardTime;
+                    mediaPlayer.seekTo((int) startTime);
+                } else {
+                    Toast.makeText(MainActivity.this, "Can't Jump Forward!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+        // rewind the music 10seconds
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int temp = (int) startTime;
+
+                if((temp - backwardTime) > 0) {
+                    /*startTime = startTime - forwardTime;*/
+                    startTime -= backwardTime;
+                    mediaPlayer.seekTo((int) startTime);
+                } else {
+                    Toast.makeText(MainActivity.this, "Can't Go Back!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
     }
+
+    // start play the music
+    private void playMusic() {
+        mediaPlayer.start();
+
+        finalTime = mediaPlayer.getDuration();
+        startTime = mediaPlayer.getCurrentPosition();
+
+        if (oneTimeOnly == 0) {
+            seekBar.setMax((int) finalTime);
+            oneTimeOnly = 1;
+        }
+
+        timeText.setText(String.format(
+                "%d min, %d sec",
+                TimeUnit.MILLISECONDS.toMinutes((long) finalTime),
+                TimeUnit.MILLISECONDS.toSeconds((long) finalTime) -
+                        TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes((long) finalTime))
+
+        ));
+
+        seekBar.setProgress((int) startTime);
+        handler.postDelayed(updateSongTime, 100);
+    }
+
+
+    // Creating the runnable for update the song
+    private Runnable updateSongTime = new Runnable() {
+        @Override
+        public void run() {
+            startTime = mediaPlayer.getCurrentPosition();
+            timeText.setText(String.format("%d min, %d sec",
+                    TimeUnit.MILLISECONDS.toMinutes((long) startTime),
+                    TimeUnit.MILLISECONDS.toSeconds((long) startTime) -
+                        TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes((long) startTime))
+                    ));
+
+            seekBar.setProgress((int) startTime);
+            handler.postDelayed(this, 100);
+        }
+    };
+
+
 }
